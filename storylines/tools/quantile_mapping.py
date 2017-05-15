4a62fe70c9d38175695054d14b531967d8f4b082
Joe Hamman <jhamman1@uw.edu>
2017-05-15 16:33:06 -0600

    quantile mapping: detrend all datasets, add reference dataset to adjust quantiles of input data

--- storylines/tools/quantile_mapping.py
+++ storylines/tools/quantile_mapping.py
@@ -5,7 +5,7 @@ import dask.array as da
 import xarray as xr
 
 
-def quantile_mapping(input_data, data_to_match, mask=None,
+def quantile_mapping(input_data, ref_data, data_to_match, mask=None,
                      alpha=0.4, beta=0.4, detrend=False,
                      extrapolate=None, n_endpoints=10):
     '''quantile mapping between `input_data` and `data_to_match`
@@ -15,6 +15,8 @@ def quantile_mapping(input_data, data_to_match, mask=None,
     input_data : xr.DataArray
         Input data to be quantile mapped to match the distribution of
         `data_to_match`
+    ref_data : xr.DataArray
+        Reference data to be used to adjust `input_data`
     data_to_match : xr.DataArray
         Target data for quantile mapping
     mask : xr.DataArray (optional, boolean)
@@ -30,7 +32,7 @@ def quantile_mapping(input_data, data_to_match, mask=None,
         end point(s) of the CDF (0, 1) will be linearly extrapolated using the
         last `n_endpoints` from the tail of the distribution. Default is None.
     n_endpoints : int
-        Number of data points to use when the `extraplate` option is set.
+        Number of data points to use when the `extrapolate` option is set.
 
     Returns
     -------
@@ -64,26 +66,31 @@ def quantile_mapping(input_data, data_to_match, mask=None,
     kwargs = dict(alpha=alpha, beta=beta, extrapolate=extrapolate,
                   n_endpoints=n_endpoints, detrend=detrend)
 
+    if ref_data is input_data:
+        ref_data = None
+
     if isinstance(input_data.data, da.Array):
         # dask arrays
         mask = mask.chunk(d0.chunks)
 
         assert input_data.data.chunks[1:] == data_to_match.data.chunks[1:]
 
-        new = da.map_blocks(_qmap_wrapper, input_data.data, data_to_match.data,
-                            mask.data, dtype=input_data.data.dtype,
+        new = da.map_blocks(_qmap_wrapper, input_data.data, ref_data.data,
+                            data_to_match.data, mask.data,
+                            dtype=input_data.data.dtype,
                             chunks=input_data.data.chunks,
                             name='qmap', **kwargs)
     else:
         # numpy arrays
-        new = _qmap_wrapper(input_data.data, data_to_match.data, mask.data,
-                            **kwargs)
+        new = _qmap_wrapper(input_data.data, ref_data.data, data_to_match.data,
+                            mask.data, **kwargs)
 
     return xr.DataArray(new, dims=input_data.dims, coords=input_data.coords,
-                        attrs=input_data.attrs, name=input_data.name)
+                        attrs=input_data.attrs,
+                        name=input_data.name).where(mask)
 
 
-def quantile_mapping_by_group(input_data, data_to_match,
+def quantile_mapping_by_group(input_data, ref_data, data_to_match,
                               grouper='time.month', **kwargs):
     '''quantile mapping between `input_data` and `data_to_match by group`
 
@@ -92,6 +99,8 @@ def quantile_mapping_by_group(input_data, data_to_match,
     input_data : xr.DataArray
         Input data to be quantile mapped to match the distribution of
         `data_to_match`
+    ref_data : xr.DataArray
+        Reference data to be used to adjust `input_data`
     data_to_match : xr.DataArray
         Target data for quantile mapping
     grouper : str, array, Grouper
@@ -118,16 +127,17 @@ def quantile_mapping_by_group(input_data, data_to_match,
 
     # Allow grouper to be None
     if grouper is None:
-        return quantile_mapping(input_data, data_to_match, **kwargs)
+        return quantile_mapping(input_data, ref_data, data_to_match, **kwargs)
 
     # Create the groupby objects
     obs_groups = data_to_match.groupby(grouper)
+    ref_groups = ref_data.groupby(grouper)
     input_groups = input_data.groupby(grouper)
 
     # Iterate over the groups, calling the quantile method function on each
     results = []
-    for (key_obs, group_obs), (key_input, group_input) in zip(obs_groups,
-                                                              input_groups):
+    for (key_obs, group_obs), (key_ref, group_ref), (key_input, group_input) \
+        in zip(obs_groups, ref_groups, input_groups):
         results.append(quantile_mapping(group_input, group_obs, **kwargs))
 
     # put the groups back together
@@ -163,37 +173,55 @@ def plotting_positions(n, alpha=0.4, beta=0.4):
     return (np.arange(1, n + 1) - alpha) / (n + 1. - alpha - beta)
 
 
-def make_x0(n_obs, n_in, alpha, beta, extrapolate):
+def make_x_and_y(y, alpha, beta, extrapolate):
     '''helper function to calculate x0, conditionally adding endpoints'''
-    temp = plotting_positions(n_obs, alpha, beta)
+    n = len(y)
 
-    if (extrapolate is not None) and (n_obs < n_in):
+    temp = plotting_positions(n, alpha, beta)
+
+    if extrapolate is not None:
         # Add endpoints to x0
         if extrapolate == 'both':
-            x0 = np.empty(n_obs + 2)
+            x = np.empty(n + 2)
+            y_new = np.full(n + 2, np.nan)
             rs = slice(1, -1)
-            x0[rs] = temp
-            x0[0] = 0.
-            x0[-1] = 1.
+            x[rs] = temp
+            x[0] = 0.
+            x[-1] = 1.
         elif extrapolate == 'min':
-            x0 = np.empty(n_obs + 1)
+            x = np.empty(n + 1)
+            y_new = np.full(n + 1, np.nan)
             rs = slice(1, None)
-            x0[rs] = temp
-            x0[0] = 0.
+            x[rs] = temp
+            x[0] = 0.
         elif extrapolate == 'max':
-            x0 = np.empty(n_obs + 1)
+            x = np.empty(n + 1)
+            y_new = np.full(n + 1, np.nan)
             rs = slice(None, -1)
-            x0[rs] = temp
-            x0[-1] = 1.
+            x[rs] = temp
+            x[-1] = 1.
         else:
             raise ValueError('unknown value for extrapolate: %s' % extrapolate)
+        # move the values from y to the new y array
+        y_new[rs] = y
     else:
-        x0 = np.empty(n_obs)
         rs = slice(None)
         extrapolate = False
-        x0 = temp
+        x = temp
+        y_new = y
 
-    return x0, rs
+    return x, y_new, rs
+
+
+def _extrapolate(y, alpha, beta, n_endpoints, how='both', ret_slice=False):
+
+    x_new, y_new, rs = make_x_and_y(y, alpha, beta, extrapolate=how)
+    y_new = calc_endpoints(x_new, y_new, how, n_endpoints)
+
+    if ret_slice:
+        return x_new, y_new, rs
+    else:
+        return x_new, y_new
 
 
 def calc_endpoints(x, y, extrapolate, n_endpoints):
@@ -204,6 +232,8 @@ def calc_endpoints(x, y, extrapolate, n_endpoints):
 
     if n_endpoints < 2:
         raise ValueError('Invalid number of n_endpoints, must be >= 2')
+
+
 
     if extrapolate in ['min', 'both']:
         s = slice(1, n_endpoints + 1)
@@ -221,50 +251,61 @@ def calc_endpoints(x, y, extrapolate, n_endpoints):
     return y
 
 
-def qmap(data, like, alpha=0.4, beta=0.4,
+def remove_trend(y, inplace=False):
+    if inplace:
+        detrended = y
+    else:
+        detrended = y.copy()
+
+    t = np.arange(len(y))
+    slope, intercept, _, _, _ = stats.linregress(t, y)
+    trend = intercept + t * slope
+    detrended -= trend
+
+    return detrended, trend
+
+
+def qmap(data, ref, like, alpha=0.4, beta=0.4,
          extrapolate=None, n_endpoints=10, detrend=None):
     '''quantile mapping for a single point'''
-    # quantiles for the input data
-    n_in = len(data)
-    x1 = plotting_positions(n_in, alpha, beta)
-
-    # quantiles for the obs
-    n_obs = len(like)
-    x0, rs = make_x0(n_obs, n_in, alpha, beta, extrapolate)
-    y0 = np.empty_like(x0)
 
     if detrend:
         # remove linear trend, saving the slope/intercepts for use later
-        t = np.arange(n_in)
-        slope, intercept, _, _, _ = stats.linregress(t, data)
-        data -= (intercept + t * slope)
+        data, data_trend = remove_trend(data, inplace=True)
+        ref, _ = remove_trend(ref, inplace=True)
+        like, _ = remove_trend(like, inplace=True)
+
+    # x is the percentiles
+    # y is the sorted data
+    sort_inds = np.argsort(data)
+    x_data, y_data, rs = _extrapolate(data[sort_inds], alpha, beta,
+                                      n_endpoints,
+                                      how=extrapolate, ret_slice=True)
+    x_ref, y_ref = _extrapolate(np.sort(ref), alpha, beta, n_endpoints,
+                                how=extrapolate)
+    x_like, y_like = _extrapolate(np.sort(like), alpha, beta,
+                                  n_endpoints, how=extrapolate)
+
+    # map the quantiles from ref-->data
+    x_data = np.interp(y_data, y_ref, x_ref)
 
     # empty array, prefilled with nans
     new = np.full_like(data, np.nan)
 
-    # Use numpy to sort
-    sort_inds = np.argsort(data, axis=0)
-
-    # Sorted Observations
-    y0[rs] = np.sort(like, axis=0)
-
-    if extrapolate:
-        # calculate endpoints
-        y0 = calc_endpoints(x0, y0, extrapolate, n_endpoints)
-
     # Indicies that would sort the input data
-    new[sort_inds] = np.interp(x1, x0, y0)
+    new[sort_inds] = np.interp(x_data, x_like, y_like)
 
     # put the trend back
     if detrend:
-        new += (intercept + t * slope)
+        new += data_trend
 
     return new
 
 
-def _qmap_wrapper(data, like, mask, **kwargs):
+def _qmap_wrapper(data, ref, like, mask, **kwargs):
     new = np.full_like(data, np.nan)
     ii, jj = np.nonzero(mask)
     for i, j in zip(ii, jj):
-        new[:, i, j] = qmap(data[:, i, j], like[:, i, j], **kwargs)
+        new[:, i, j] = qmap(data[:, i, j], ref[:, i, j], like[:, i, j],
+                            **kwargs)
     return new
